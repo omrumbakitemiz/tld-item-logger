d2c0334fb80205a96b189619f58638de4136d183
Ömrüm Baki Temiz <omrumbakitemiz@icloud.com>
2023-08-20 14:16:00 +0300

    TLD-29 example react button added

---
+++ src/integrations/react/index.tsx
@@ -0,0 +1,6 @@
+/** @jsxImportSource react */
+
+import { qwikify$ } from '@builder.io/qwik-react';
+import { Button as ButtonReact } from '~/integrations/react/ui/button';
+
+export const Button = qwikify$(ButtonReact);
--- src/routes/react/index.tsx
+++ src/routes/react/index.tsx
@@ -1,13 +1,26 @@
-import { component$ } from '@builder.io/qwik';
+import { component$, useSignal } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
-import { Immino } from '~/integrations/react/immino';
+import { Button } from '~/integrations/react';
 
 export default component$(() => {
+  console.log('Qwik Render');
+  const count = useSignal(0);
+
   return (
-    <>
+    <div class="flex flex-col gap-4 items-center">
       <h1 class="text-2xl text-emerald-400">Qwik/React this is React component</h1>
-      <Immino client:visible></Immino>
-    </>
+      <Button
+        onClick$={() => {
+          console.log('button clicked');
+          count.value++;
+        }}
+        client:visible
+      >
+        Armut
+      </Button>
+
+      <div class="text-2xl text-red-400">Count: {count}</div>
+    </div>
   );
 });
 
